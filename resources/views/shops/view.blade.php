2f272fde2c2d99e7acd9844961c925d45bd554b1
Saddam Azy <myxzlpltk@gmail.com>
2021-04-12 08:42:01 +0700

    revert luminance class view

--- resources/views/shops/view.blade.php
+++ resources/views/shops/view.blade.php
@@ -16,20 +16,20 @@
 					<img src="{{ $shop->logo_path }}" alt="" style="height: 60px;width: 60px;">
 				</div>
 				<div class="listing__hero__text">
-					<h2 class="{{ $shop->luminance_class }}">{{ $shop->name }}</h2>
+					<h2 class="text-light">{{ $shop->name }}</h2>
 					<div class="listing__hero__widget">
 						@if($shop->public_reviews_avg_rating)
-						<div class="listing__hero__widget__rating {{ $shop->luminance_class }}">
+						<div class="listing__hero__widget__rating text-light">
 							{!! \App\Helpers\Helper::rating($shop->public_reviews_avg_rating) !!}
 						</div>
-						<div class="{{ $shop->luminance_class }}">{{ $shop->public_reviews_count }} Review</div>
+						<div class="text-light">{{ $shop->public_reviews_count }} Review</div>
 						@else
-							<p class="{{ $shop->luminance_class }}">Belum ada rating</p>
+							<p class="text-light">Belum ada rating</p>
 						@endif
 					</div>
-					<p class="{{ $shop->luminance_class }}">
-						<span class="icon_pin_alt {{ $shop->luminance_class }}"></span> {{ $shop->address }}<br>
-						<span class="icon_currency {{ $shop->luminance_class }}"></span> {{ $shop->price_range }}
+					<p class="text-light">
+						<span class="icon_pin_alt text-light"></span> {{ $shop->address }}<br>
+						<span class="icon_currency text-light"></span> {{ $shop->price_range }}
 					</p>
 				</div>
 			</div>

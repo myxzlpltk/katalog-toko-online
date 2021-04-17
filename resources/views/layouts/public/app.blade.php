6d7df42699175b17b24d2156a26b95e95afde44a
Saddam Azy <myxzlpltk@gmail.com>
2021-04-17 19:33:15 +0700

    fix seo not found

--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	{!! SEO::generate() !!}
+	{!! \Artesaos\SEOTools\Facades\SEOTools::generate() !!}
 
 	<title>{{ config('app.name') }} - @yield('title', 'Halaman Kosong')</title>
 

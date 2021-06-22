4719ded26d796a36c85931980ce0e4f9f5f030bd
Saddam Azy <myxzlpltk@gmail.com>
2021-06-22 16:06:45 +0700

    update redirect routing

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller{
 
 	public function dashboard(Request $request){
 		if($request->user()->is_admin){
 			return redirect()->route('admin.dashboard');
+		}
+		elseif($request->user()->is_dosen){
+			return redirect()->route('dosen.dashboard');
+		}
+		elseif($request->user()->is_mahasiswa){
+			return redirect()->route('dosen.dashboard');
+		}
+		else{
+			Auth::logout();
+
+			return redirect()->route('login');
 		}
 	}
 }

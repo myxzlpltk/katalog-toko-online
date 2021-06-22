f9e5c725835116b0416fff943f361e89293063f0
Saddam Azy <myxzlpltk@gmail.com>
2021-06-22 15:48:13 +0700

    create empty policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Dosen;
+use App\Models\Photo;
+use App\Models\Review;
+use App\Models\Shop;
+use App\Policies\CategoryPolicy;
+use App\Policies\DosenPolicy;
+use App\Policies\PhotoPolicy;
+use App\Policies\ReviewPolicy;
+use App\Policies\ShopPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +23,11 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Category::class => CategoryPolicy::class,
+		Dosen::class => DosenPolicy::class,
+		Photo::class => PhotoPolicy::class,
+		Review::class => ReviewPolicy::class,
+		Shop::class => ShopPolicy::class,
     ];
 
     /**
